a863d64d167bf3109a6a36843cd9eadecb94d894
Marconi Moreto <caketoad@gmail.com>
2011-11-26 00:00:43 +0800

    initial commit

--- devstream/models/user.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from mongoengine import *
-
-
-class User(Document):
-    email = StringField(required=True)
-    password = StringField(required=True)
--- devstream/views/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def add_routes(config):
-    config.add_route('socket_io', 'socket.io/*remaining')
-    config.add_route('home', '/')
-
-    # status
-    config.add_route('post_status', '/status/post')
